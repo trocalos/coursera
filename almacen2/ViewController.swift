2a07a626657be9663ed4a808d56b89685b73f29d
Diana Guill hernandez <dianaguillhernandez@gmail.com>
2016-02-04 20:57:28 +0100

    Initial Commit

---
+++ almacen2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  almacen2
+//
+//  Created by Diana Guill on 4/2/16.
+//  Copyright © 2016 Diana Guill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
